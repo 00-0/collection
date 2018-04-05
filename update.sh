192404f6a5d624709202ec1ec97b273e1d76b615
00-0 <paulo.mouat+00-0@gmail.com>
2018-04-05 11:36:54 -0400

    Added baseline.

---
+++ update.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+git checkout master
+git pull
+rsync -r --exclude="update.sh" ../../collection .
+git add -A
+git commit
+git push
+git checkout gh-pages
+git pull
+git merge -m "Merged latest from master" master
+git push
+git checkout master
